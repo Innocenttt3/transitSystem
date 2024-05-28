40d32927c862df66973fc886ff4e0b7e32ecf689
Osariusz <osariusz@interia.pl>
2024-05-28 13:04:21 +0200

    Fix

--- spedytor/transitSystem/src/main/java/org/kamilG/Facade.java
+++ spedytor/transitSystem/src/main/java/org/kamilG/Facade.java
@@ -13,23 +13,6 @@ public class Facade extends ItemHolder {
   List<Vehicle> vehicles = new ArrayList<>();
   List<Person> persons = new ArrayList<>();
 
-  Facade() {
-    this.setItems(
-        new ArrayList<>(Arrays.asList(
-            new Item("Coders' tears", 105253),
-            new Item("Papieska Kremówka", 2137)))
-    );
-
-    this.persons = new ArrayList<>(Arrays.asList(
-            new Person("Andrzej","Duda", LocalDate.of(1972,5,16), LocalDate.of(1972,5,16), Person.TypeOfEmployment.PART_TIME)
-    ));
-
-    this.vehicles = new ArrayList<>(Arrays.asList(new Truck(15, 1939, 1234, 4321, Vehicle.FuelType.PB, "Uzbekistan", "Dacia", "Logan")));
-
-    this.warehouses =
-        new ArrayList<>(Arrays.asList(new Warehouse("Berlin", new ArrayList<>(List.of(new Item("Michas", 60)))), new Warehouse("Moskwa", new ArrayList<>())));
-  }
-
   public void addWarehouse(Warehouse warehouse) {
     warehouses.add(warehouse);
   }
